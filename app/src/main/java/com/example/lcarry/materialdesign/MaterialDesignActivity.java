ca9e4f744fd32c20e90d2ab6b3a75e92ccedba36
lcarry <canghaiyisu0212@sina.com>
2017-06-02 09:01:06 +0800

    materialDesign的activity上传

---
+++ app/src/main/java/com/example/lcarry/materialdesign/MaterialDesignActivity.java
@@ -0,0 +1,15 @@
+package com.example.lcarry.materialdesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.lcarry.uiwidgettest.R;
+
+public class MaterialDesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design);
+    }
+}
